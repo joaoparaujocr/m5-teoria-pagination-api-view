e8c95d19bd3a9174ba066edc8a413b44dcf8d84b
João Araújo <61572306+joaoparaujocr@users.noreply.github.com>
2022-12-17 13:58:07 -0500

    Initial commit

---
+++ musicians/views.py
@@ -0,0 +1,21 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from musicians.models import Musician
+from musicians.serializers import MusicianSerializer
+
+
+class MusicianView(APIView):        
+    def post(self, request):
+        serializer = MusicianSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        
+        musician = Musician.objects.create(**serializer.validated_data)
+        serializer = MusicianSerializer(musician)
+        
+        return Response(serializer.data, 201)
+    
+    def get(self, request):
+        musicians = Musician.objects.all()
+        serializer = MusicianSerializer(musicians, many=True)
+        
+        return Response(serializer.data)
